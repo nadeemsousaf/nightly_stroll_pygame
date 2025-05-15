669924fefe5ea6ca60504e8ca2f178c4fef53d92
nadeemsousaf <nmsou6754@gmail.com>
2025-05-15 13:12:03 -0400

    preparing

--- state.py
+++ state.py
@@ -1,6 +1,6 @@
 import pygame
 from sys import exit
-import random
+from person import *
 
 pygame.init()
 window = pygame.display.set_mode((800,500))
@@ -8,3 +8,36 @@ pygame.display.set_caption('Nightly Stroll')
 clock = pygame.time.Clock()
 
 game_state = 0 #keeps track of game location
+player = Player(0,0,0,0)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_DOWN:
+                player.y_dir = 1
+                player.show = player.front
+            elif event.key == pygame.K_UP:
+                player.y_dir = -1
+            elif event.key == pygame.K_LEFT:
+                player.x_dir = -1
+                player.show = player.left
+            elif event.key == pygame.K_RIGHT:
+                player.x_dir = 1
+                player.show = player.right
+            elif event.key == pygame.K_SPACE:
+                player.attack()
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_DOWN:
+                player.y_dir = 0
+            elif event.key == pygame.K_UP:
+                player.y_dir = 0
+            elif event.key == pygame.K_LEFT:
+                player.x_dir = 0
+            elif event.key == pygame.K_RIGHT:
+                player.x_dir = 0
+
+    pygame.display.update()
+    clock.tick(40)
