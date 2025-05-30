27695888f75d2f61667d8dea6a162663469b7fe5
nadeemsousaf <nmsou6754@gmail.com>
2025-05-30 11:12:27 -0400

    sprite

--- obj_class.py
+++ obj_class.py
@@ -1,13 +1,25 @@
 import pygame
 
-class Player():
-    def __init__(self, front, left, right):
-        self.show = front
-        self.front = front
-        self.left = left
-        self.right = right
-        self.x = 0
-        self.y = 245
+class CustSprite():
+    def __init__(self,x,y,normal_img):
+        self.rect = pygame.Rect(x,y,x+normal_img.get_width(),y+normal_img.get_height()) #height goes positive downward...?, width & height catered to normal state
+        self.normal_img = normal_img
+        self.show_img = normal_img
+    def get_top_left(self):
+        return self.rect.topleft
+    def get_top_right(self):
+        return self.rect.topright
+    def get_bottom_left(self):
+        return self.rect.bottomleft
+    def get_bottom_right(self):
+        return self.rect.bottomright
+
+class Player(CustSprite):
+    def __init__(self, front_img, left_img, right_img):
+        super().__init__(0,245,front_img)
+        self.front_img = front_img
+        self.left_img = left_img
+        self.right_img = right_img
         self.x_dir = 0
         self.y_dir = 0
         self.speed = 10
@@ -20,37 +32,34 @@ class Action(): #if npc moves and/or speaks
         self.dialogue = dialogue
         self.movement = movement
 
-class NPC():
-    def __init__(self, x, y, normal_img, actions):
+class NPC(CustSprite):
+    def __init__(self,x,y,front_img,left_img,right_img,actions):
+        super().__init__(x,y,front_img)
         self.x = x
         self.y = y
-        self.show = normal_img
-        self.normal_img = normal_img
+        self.front_img = front_img
+        self.left_img = left_img
+        self.right_img = right_img
         self.actions = actions #singly-linked list of action(s)
-        self.health = 100
+        self.health = 100 #maybe necessary
     def draw(self):
         print("placeholder")
     def motion(self): #speak and/or move during user interaction
         print("placeholder")
 
-class TextBubble(): #unsure if will require images
-    def __init__(self,text,x,y,font,fg,bg):
+class TextBubble(CustSprite): #unsure if will require images
+    def __init__(self,text,x,y,font,normal_img):
+        super().__init__(x,y,normal_img)
         self.text = text
-        self.x = x
-        self.y = y
         self.font = font
-        self.fg = fg
-        self.bg = bg
     def draw(self):
         print("placeholder")
 
-class Button(): #likely will require images, needs hover and click detection
+class Button(CustSprite): #likely will require images, needs hover and click detection
     def __init__(self,text,x,y,font,normal_img,hover_img,click_img):
+        super().__init__(x,y,normal_img)
         self.text = text
-        self.rect = pygame.Rect(x,y,x+normal_img.get_width(),y+normal_img.get_height()) #height goes positive downward...?
         self.font = font
-        self.normal_img = normal_img
-        self.show_img = normal_img
         self.click_img = click_img
         self.hover_img = hover_img
         self.hover = False
@@ -71,14 +80,6 @@ class Button(): #likely will require images, needs hover and click detection
                 if self.hover == True:
                     self.show_img = self.click_img
                     self.click = True #triggers an event, more code to come
-    def get_top_left(self): #check if gets are necessary, possible parent class for all classes with rect
-        return self.rect.topleft
-    def get_top_right(self):
-        return self.rect.topright
-    def get_bottom_left(self):
-        return self.rect.bottomleft
-    def get_bottom_right(self):
-        return self.rect.bottomright
 
 class NodeL(): #for singly-linked list (events by scene & npc actions)
     def __init__(self,data):
@@ -100,7 +101,7 @@ class NodeT(): #for tree with no built-in reversal (storyline/map)
     def set_forward(self,f):
         self.f = f
 
-class State(): #requires list of objects to render on scene? (on top of fg,mg,bg images)
+class State(): #requires list of objects to render on scene? (on top of fg,mg,bg images)- sprite? no...
     def __init__(self,number,bg,mg,fg,exit_x,current):
         self.number = number
         self.bg = bg
