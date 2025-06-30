7796eb2926808748c7d882f56ef8a2cb6f90770f
nadeemsousaf <nmsou6754@gmail.com>
2025-06-30 11:46:40 -0400

    func

--- button_functions.py
+++ button_functions.py
@@ -1,6 +1,17 @@
-from flow import global_dict
+from flow import global_dict, mem_file
 
+#make specific lambdas to add specific items per button and pass to button class
 def add_to_inventory(name,item):
     global_dict[name] = item
 
-#make specific lambdas to add specific items per button and pass to button class
+def restart():
+    x = 10
+    #reset the dict and write it to file
+
+def resume():
+    x = 10
+    #maybe not needed, could just pass the state inside the saved dict
+
+def quit():
+    x = 10
+    #can we end program from here?
